79be7bf96ef1e92a71e768f09fb3eaf04bf088a5
Rafael Manresa <rmanresa@gmail.com>
2021-03-01 16:25:17 +0100

    Entrega inicial: Proycyo Maven

---
+++ src/main/java/ManresaMurcia_Rafael_eventos_1/practica/App.java
@@ -0,0 +1,13 @@
+package ManresaMurcia_Rafael_eventos_1.practica;
+
+/**
+ * Entrega 1 con proyecto Maven
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Entrega 1 con proyecto Maven" );
+    }
+}
